a7762c446c7c6006bebb45defc24ce1323997bc2
Jubair <jubair.ahmed2838@gmail.com>
2024-01-22 01:15:46 +0600

    Add: database connected

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose")
 require("dotenv").config();
 
+const uri = process.env.dbURI
+
+const connectDB = async()=>{
+    console.log("Connecting to database..")
+    await mongoose.connect(uri, {dbName: "fitTrackDB"})
+    console.log("Connected to database")
+}
+
+module.exports= connectDB
