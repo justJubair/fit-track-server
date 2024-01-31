ac62d5f0a51b8b3b6034ccadf2de121ec30ce126
Aurnab <zubayeraurnab@gmail.com>
2024-01-31 11:09:56 +0600

    Like Patch  Api created

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const postBlogs = require("../../api/blogs/postBlogs");
 const getBlogs = require("../../api/blogs/getBlogs");
+const patchLikes = require("../../api/blogs/patchLikes");
 const router = express.Router();
 
 //post Blogs to DB
 router.post("/api/v1/blogs", postBlogs);
 router.get("/api/v1/all-blogs", getBlogs);
+router.patch("/api/v1/like", patchLikes);
 
 module.exports = router;
