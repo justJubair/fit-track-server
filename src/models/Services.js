26bd075db97e1bbef4cc261eab9c2d864cb31546
Jubair <jubair.ahmed2838@gmail.com>
2024-02-01 15:42:15 +0600

    Update: service model updated

--- src/models/Services.js
+++ src/models/Services.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose")
 const {Schema} = mongoose;
 
 const ServiceSchema = new Schema({
-    title: String,
-    url: String,
+    heading: String,
+    subheading: String,
     thumbnail: String,
+    serviceId: String,
+    duration: Number,
+    category: String,
+    numberOfVideos: Number,
+    videos: [
+        {
+           title: String,
+           vlink: String, 
+        }
+    ]
 })
 
 const Services = mongoose.model("Services", ServiceSchema)
