6c48ff5854083d7191a68cf7ac196ef6f98ed1fe
Aurnab <zubayeraurnab@gmail.com>
2024-02-21 19:32:16 +0600

    user join room api created

--- src/WebRTC/index.js
+++ src/WebRTC/index.js
@@ -1,5 +1,7 @@
 const { Server } = require("socket.io");
-const io = Server(5000);
+const io = new Server(8000, {
+  cors: true,
+});
 
 io.on("connection", (socket) => {
   console.log(`Socket connection: ${socket.id}`);
