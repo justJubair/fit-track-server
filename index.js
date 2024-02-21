9cedae6a606d8d699d2e4ee8966b24e2c4978e3b
Aurnab <zubayeraurnab@gmail.com>
2024-02-21 22:08:01 +0600

    Call accept api created

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ const main = async () => {
     socket.on("user:call", ({ to, offer }) => {
       io.to(to).emit("incoming:call", { from: socket.id, offer });
     });
+
+    socket.on("call:accepted", ({ to, ans }) => {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
   });
 
   server.listen(port, () => {
